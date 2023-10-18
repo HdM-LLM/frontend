1b55bdbd65807e322de447885e24e4b28cc07b31
Max Tyrchan <matyrchan@googlemail.com>
2023-10-18 15:31:22 +0200

    Finally "Hello World" appears :)

--- src/testFetch.tsx
+++ src/testFetch.tsx
@@ -6,11 +6,9 @@ export async function fetchData(url: string): Promise<any> {
   }
   const contentType = res.headers.get("Content-Type");
   if (contentType && contentType.indexOf("application/json") !== -1) {
-    console.log(res.json());
     return res.json();
   }
   if (contentType && contentType.indexOf("text") !== -1) {
-    console.log(res.text());
     return res.text();
   }
   throw new Error("Unsupported content type");
