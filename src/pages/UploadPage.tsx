fa1dbd8b98904794125e89b92fc21be126689a26
Max Tyrchan <matyrchan@googlemail.com>
2023-11-16 16:57:56 +0100

    implemented pdf upload

--- src/pages/UploadPage.tsx
+++ src/pages/UploadPage.tsx
@@ -1,10 +1,45 @@
-import React from "react";
+import * as React from "react";
 import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
 
 export default function UploadPage() {
+  const [cover_letter, setCoverLetter] = React.useState<File | null>(null);
+  const [resume, setResume] = React.useState<File | null>(null);
+
   return (
     <Container maxWidth="lg" sx={{ mt: 10, mb: 4 }}>
-      <h1>UploadPage</h1>
+      <h1>File Upload</h1>
+      <Box sx={{ display: "flex", mb: 2 }}>
+        <h4>Cover Letter</h4>
+        <Button variant="contained" component="label" sx={{ ml: 5 }}>
+          Upload
+          <input
+            type="file"
+            accept="application/pdf"
+            hidden
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              if (!event.target.files) return;
+              setCoverLetter(event.target.files[0]);
+            }}
+          />
+        </Button>
+      </Box>
+      <Box sx={{ display: "flex" }}>
+        <h4>Resume</h4>
+        <Button variant="contained" component="label" sx={{ ml: 5 }}>
+          Upload
+          <input
+            type="file"
+            accept="application/pdf"
+            hidden
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              if (!event.target.files) return;
+              setResume(event.target.files[0]);
+            }}
+          />
+        </Button>
+      </Box>
     </Container>
   );
 }
