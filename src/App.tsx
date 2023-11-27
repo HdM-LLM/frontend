b935a99827f1c2085897fca3bb8196d6dee85390
Nicole Sauer <95630400+NicoleSauer@users.noreply.github.com>
2023-11-27 13:45:49 +0100

    re-formatted every file for consistent formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,29 @@
-import * as React from "react";
-import Dashboard from "./pages/Dashboard";
-import ApplicantsPage from "./pages/ApplicantsPage";
-import OverviewPage from "./pages/OverviewPage";
-import UploadPage from "./pages/UploadPage";
-import InquiriesPage from "./pages/InquiriesPage";
-import PositionPage from "./pages/PositionPage";
-import SettingsPage from "./pages/SettingsPage";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Box from "@mui/material/Box";
-import AppBar from "./components/AppBar";
-import Drawer from "./components/Drawer";
-import { styled } from "@mui/material";
+import * as React from 'react';
+import Dashboard from './pages/Dashboard';
+import ApplicantsPage from './pages/ApplicantsPage';
+import OverviewPage from './pages/OverviewPage';
+import UploadPage from './pages/UploadPage';
+import InquiriesPage from './pages/InquiriesPage';
+import PositionPage from './pages/PositionPage';
+import SettingsPage from './pages/SettingsPage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import AppBar from './components/AppBar';
+import Drawer from './components/Drawer';
+import { styled } from '@mui/material';
 
 export default function App() {
   const MainBox = styled(Box)(({ theme }) => ({
     flexGrow: 1,
-    height: "100dvh",
-    overflow: "auto",
+    height: '100dvh',
+    overflow: 'auto',
     background: theme.palette.background.default,
-    boxShadow: "none",
+    boxShadow: 'none',
     marginLeft: theme.spacing(28),
   }));
   return (
     <BrowserRouter>
-      <Box sx={{ display: "flex" }}>
+      <Box sx={{ display: 'flex' }}>
         <AppBar />
         <Drawer />
         <MainBox component="main">
